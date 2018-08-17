8541b52266354c0c045ed9ab252d9e2a4330844c
Djumic Branislav <djumicbranislav@outlook.com>
2018-08-17 15:23:49 +0200

    Bug Fixes

--- src/components/receive.jsx
+++ src/components/receive.jsx
@@ -4,16 +4,18 @@ import Axios from "axios";
 
 export default class Receive extends Component {
   state = {
-    walletID: "djklghha2412sdiai12sdafUOUSYF7&f0418989"
+    walletID: "2N4CTAPvmm5SrryubndTskMcLRrSJWdKsRG"
   };
 
   render() {
     return (
       <div>
         <p className="message">This is your receive address!</p>
-        <div className="receiveBody">
-          <p className="id">{this.state.walletID}</p>
-        </div>
+        <p className="id">{this.state.walletID}</p>
+        <p className="explain">
+          *When you want someone to send you funds you give them your receive
+          address!
+        </p>
       </div>
     );
   }
