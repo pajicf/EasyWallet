3df0d59b7e276532c0058d81293c1496d67f7121
Djumic Branislav <djumicbranislav@outlook.com>
2018-08-17 13:51:48 +0200

    Bug fixes

--- src/components/receive.jsx
+++ src/components/receive.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../css/receive.css";
 import Axios from "axios";
 
-export default class receive extends Component {
+export default class Receive extends Component {
   state = {
     walletID: "djklghha2412sdiai12sdafUOUSYF7&f0418989"
   };
@@ -10,8 +10,8 @@ export default class receive extends Component {
   render() {
     return (
       <div className="receiveBody">
-        <p calssName="">You need to give this to person paying you!</p>
-        <p className="">{this.state.walletID}</p>
+        <p class="message">You need to give this to person paying you!</p>
+        <p className="id">{this.state.walletID}</p>
       </div>
     );
   }
