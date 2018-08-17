9d0da4e407dd223412273c9265c8d030a105ad64
Filip <fica.pajic@gmail.com>
2018-08-17 09:42:09 +0200

    Modified the code so that now you can have multiple pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import logo from "./images/logo.png";
-import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./css/login.css";
+
+import LogIn from "./components/login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="headerText"> Easy Wallet</p>
-        </header>
-        <div className="box">
-          <input className="wallID" placeholder="Enter your ID" type="text" />
-          <div className="buttonsBox">
-            <button style={{ backgroundColor: "#00adb5" }} className="btnL">
-              Log in
-            </button>
-            <button className="btnL">New wallet</button>
-          </div>
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path="/" render={() => <LogIn />} />
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import logo from "../images/logo.png";
+
+export default class logIn extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="headerText"> Easy Wallet</p>
+        </header>
+        <div className="box">
+          <input className="wallID" placeholder="Enter your ID" type="text" />
+          <div className="buttonsBox">
+            <button style={{ backgroundColor: "#00adb5" }} className="btnL">
+              Log in
+            </button>
+            <button className="btnL">New wallet</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
