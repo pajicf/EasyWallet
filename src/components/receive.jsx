56b78c34f8606e4a608a409b9c622178558e358b
Filip <fica.pajic@gmail.com>
2018-08-21 14:30:34 +0200

    Receive now works

--- src/components/receive.jsx
+++ src/components/receive.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import "../css/receive.css";
+import Axios from "axios";
 
 export default class Receive extends Component {
   state = {
-    walletID: "2N4CTAPvmm5SrryubndTskMcLRrSJWdKsRG"
+    walletID: "",
+    recAddress: ""
+  };
+
+  componentWillMount() {
+    this.setState({ walletID: this.props.wallID });
+  }
+  componentDidMount() {
+    this.getAdd();
+  }
+
+  getAdd = () => {
+    console.log(this.state.walletID);
+    Axios.get(`http://localhost:8080/wallet/${this.state.walletID}`).then(
+      res => {
+        console.dir(res);
+        this.setState({ recAddress: res.data.receiveAddress.address });
+      }
+    );
   };
 
   render() {
     return (
       <div className="receiveBody">
         <p className="message">This is your receive address!</p>
-        <div className="id">{this.state.walletID}</div>
+        <div className="id">{this.state.recAddress}</div>
         <p className="explain">
           *When you want someone to send you funds you give them your receive
           address!
