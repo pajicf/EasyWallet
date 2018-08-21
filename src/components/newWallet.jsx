b40b5643fbfd2b4a2bebeeff8c83091535b602de
Djumic Branislav <djumicbranislav@outlook.com>
2018-08-21 11:30:48 +0200

    Adressed some life quality issues and POST request for creating wallet added

--- src/components/newWallet.jsx
+++ src/components/newWallet.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import Axios from "axios";
 import logo from "../images/logo.png";
 import "../css/newWallet.css";
 
 export default class newWallet extends Component {
-  state = {};
+  state = {
+    wallet: {}
+  };
+
+  componentDidMount() {
+    this.generateNewWallet();
+  }
+
+  generateNewWallet = () => {
+    Axios.post("http://localhost:8080/wallet", "REQUEST")
+      .then(res => {
+        let newWallet = res.data;
+        this.setState({ wallet: newWallet });
+      })
+      .catch(error => {
+        this.setState({
+          wallet: { id: "Error has occured! Try later." }
+        });
+        document.getElementById("warning").style.display = "none";
+      });
+  };
 
   render() {
     return (
@@ -13,13 +34,13 @@ export default class newWallet extends Component {
           <p className="headerText"> Easy Wallet</p>
         </header>
         <div className="box">
-          <p className="warning">
-            CAUTION: This is your wallet ID, don't lose it or give it to anyone.{" "}
-            <br /> If you lose it your money may get stolen.
+          <p className="warning" id="warning">
+            CAUTION:
+            <br /> This is your wallet ID, don't lose it or give it to anyone.
+            <br /> If you lose it your money might be stolen and you won't be
+            able to recover it.
           </p>
-          <div className="newWalletID">
-            v2x098b41c78c2232e8b732dd0958a9bec34f29b54155d145496d69d4cb0e76cd33
-          </div>
+          <div className="newWalletID">{this.state.wallet.id}</div>
           <p className="centerText">Double click on ID then copy it</p>
           <a href="/">
             <button className="btnL" style={{ backgroundColor: "#00adb5" }}>
