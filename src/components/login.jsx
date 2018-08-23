9f8f65a19d0f9f7cd1625a238da78a0b9f0522d6
Filip <fica.pajic@gmail.com>
2018-08-23 12:30:31 +0200

    Now you can choose which coin to log in with

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router";
 import logo from "../images/logo.png";
+import ltc from "../images/litecoin.svg";
+import btc from "../images/btc.png";
 import "../css/login.css";
 import Axios from "axios";
 
@@ -11,6 +13,12 @@ export default class logIn extends Component {
     redirect: false,
     coin: "tbtc"
   };
+
+  componentDidMount() {
+    document.getElementById("btnType1").style.backgroundColor = "#fd9200";
+    document.getElementById("btnType1").style.boxShadow = "0 0 60px #faa02a";
+    document.getElementById("btnType1").style.flexGrow = "2";
+  }
 
   handleChange = data => {
     this.setState({ id: data });
@@ -35,6 +43,24 @@ export default class logIn extends Component {
       });
   };
 
+  handleType(but) {
+    if (but === 1) {
+      document.getElementById("btnType2").style.backgroundColor =
+        "rgba(0,0,0,0)";
+      document.getElementById("btnType2").style.boxShadow = "none";
+      document.getElementById("btnType1").style.backgroundColor = "#fd9200";
+      document.getElementById("btnType1").style.boxShadow = "0 0 60px #faa02a";
+      this.setState({ coin: "tbtc" });
+    } else {
+      document.getElementById("btnType1").style.backgroundColor =
+        "rgba(0,0,0,0)";
+      document.getElementById("btnType1").style.boxShadow = "none";
+      document.getElementById("btnType2").style.backgroundColor = "#bdbdbd";
+      document.getElementById("btnType2").style.boxShadow = "0 0 60px #747272";
+      this.setState({ coin: "tltc" });
+    }
+  }
+
   render() {
     if (this.state.redirect) {
       return <Redirect push to="/home" />;
@@ -47,6 +73,23 @@ export default class logIn extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p className="headerText"> Easy Wallet</p>
         </header>
+        <div className="coinType">
+          <p className="chooseType"> Which curency do you want to use?</p>
+          <button
+            id="btnType1"
+            className="bitCoinBtn"
+            onClick={() => this.handleType(1)}
+          >
+            <img alt="img1" width="128px" height="128px" src={btc} />
+          </button>
+          <button
+            id="btnType2"
+            className="liteCoinBtn"
+            onClick={() => this.handleType(2)}
+          >
+            <img alt="img2" width="128px" height="128px" src={ltc} />
+          </button>
+        </div>
         <div className="box">
           <input
             id="ID"
