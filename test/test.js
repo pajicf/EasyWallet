eb54c6c9333cc331a68c233c41aa685d1ec93a52
MFaith <40504666+MihailoRistic@users.noreply.github.com>
2018-08-29 12:47:18 +0200

    Added gen. wallet test to back

--- test/test.js
+++ test/test.js
@@ -8,6 +8,23 @@ const base_url = "http://localhost:8080/wallet";
 
 chai.use(chaiHttp);
 describe("Wallet Routes", () => {
+  describe("Make Wallet", () => {
+    it("Should make a wallet", done => {
+      let telo = {
+        coin: coin,
+        label: "npm run test"
+      };
+      chai
+        .request(base_url)
+        .post(``)
+        .send(telo)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
   describe("Get Wallet", () => {
     it("Should get a wallet", done => {
       chai
