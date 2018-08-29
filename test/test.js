1c26eb88657fa91d6830782feb2dd1990079f454
MFaith <40504666+MihailoRistic@users.noreply.github.com>
2018-08-29 12:23:59 +0200

    added npm run test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
-var should = require("should");
-var expect = require("chai").expect;
-var baseUrl = "http://localhost:8080";
-var util = require("util");
-var router=require('../BackEnd/api/routes/walletRoutes');
+const chai = require("chai");
+require("dotenv").config();
+const chaiHttp = require("chai-http");
+const should = chai.should();
+const id = process.env.ID;
+const coin = process.env.COIN;
+const base_url = "http://localhost:8080/wallet";
 
-
-describe('tltc', function() {
-    it('returns wallet', function(done) {
-        this.timeout(20000);
-        router.get(`${baseUrl}/wallet?id=${process.env.TEST_TLTC_ID}&coin=tltc`,
-            function(error, response, body) 
-            {
-                    console.log(error);
-                    expect(response.statusCode).to.exist();
-                    console.log(body);
-                    done();
-            });
-            console.log("Zelim da umrem");
-            
+chai.use(chaiHttp);
+describe("Wallet Routes", () => {
+  describe("Get Wallet", () => {
+    it("Should get a wallet", done => {
+      chai
+        .request(base_url)
+        .get(`?id=${id}&coin=${coin}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("id").eql(id);
+          done();
+        });
     });
+  });
 });
