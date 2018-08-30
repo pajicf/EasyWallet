d5f4af94e9a234271dc60d736dd577e73b9d5d25
Filip <fica.pajic@gmail.com>
2018-08-30 12:07:49 +0200

    Added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import App from "./App";
+import Login from "./components/login";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("If log in button changes text when pressed", () => {
+  let props = {
+    chngWallId: jest.fn(),
+    chngWallCoin: jest.fn()
+  };
+  let logInScreen = shallow(<Login {...props} />);
+
+  expect(logInScreen.find("#btnLID").text()).toEqual("Log in");
+
+  logInScreen.find("#btnLID").simulate("click");
+
+  expect(logInScreen.find("#btnLID").text()).toEqual("Logging in");
 });
